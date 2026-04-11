e2b20836d5eb0c4939152fe5fa1f7cb1c10ad0a8
abualnour <33073799+abualnour@users.noreply.github.com>
2026-04-11 06:27:33 +0300

    Serve media files from disk in production

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.static import serve
 
 from core.views import BackupCenterView, DashboardHomeView
 from .views import system_landing
@@ -16,4 +16,6 @@ urlpatterns = [
     path("employees/", include("employees.urls")),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
